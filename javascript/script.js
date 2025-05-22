e1bb727bb3a4c265d9ccd80a3826e3e78c403770
accioly1 <magnavita.guilherme@gmail.com>
2025-05-21 23:51:45 -0300

    feat: adiciona rolagem automática imagens carrossel

--- javascript/script.js
+++ javascript/script.js
@@ -6,3 +6,22 @@ $('#mobile_btn').find('i').toggleClass('fa-x')
 });
 });
 /*Código para abertura da barra de navegação no mobile*/
+
+ 
+/*Código para rolagem das imagens no Carrossel*/ 
+    let count = 1;
+    document.getElementById("radio1").checked = true;
+    
+    setInterval(function() {
+    nextImage()
+    }, 4000)
+    
+    function nextImage(){
+        count++;
+        if(count>4){
+            count = 1;
+        }
+    
+        document.getElementById("radio"+count).checked = true;
+    }    
+    /*Código para rolagem das imagens no Carrossel*/ 
